d9f17c94d46aab1be7fe2cbe432e09f55e212df0
Haugjen <mortengoericke@gmail.com>
2018-09-17 12:51:58 +0200

    added actual content

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyMathDLLTurorial;
 
 namespace UsingForeignDLL
 {
@@ -10,7 +11,18 @@ namespace UsingForeignDLL
     {
         static void Main(string[] args)
         {
-
+            MathMachine mm = new MathMachine();
+            Console.WriteLine("Adding 2 and 3");
+            Console.WriteLine(mm.Add(2,3));
+            Console.WriteLine();
+            Console.WriteLine("Dividing 2 by 0!! :(");
+            Console.WriteLine(mm.Divide(2,0));
+            Console.WriteLine();
+            Console.WriteLine("Multiplying 2 by 3");
+            Console.WriteLine(mm.Multiply(2,3));
+            Console.WriteLine();
+            Console.WriteLine("subtracting 3 from 2");
+            Console.WriteLine(mm.Subract(2,3));
             Console.ReadLine();
         }
     }
